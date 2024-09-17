44419f877c2dce624e0fd4f797fbdc3ed8fbb9d5
shamilshaz03 <93109099+shamilshaz03@users.noreply.github.com>
2024-09-17 14:47:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
-# Check if UPSTREAM_REPO is set, otherwise clone the main repository
-if [ -z $UPSTREAM_REPO ]
+if [ -z "$UPSTREAM_REPO" ]
 then
-  echo "Cloning main Repository"
-  git clone https://github.com/shamilshaz03/telagram-bot.git /telegram-bot
+  echo "Cloning main repository..."
+  git clone https://github.com/shamilshaz03/telagram-bot.git /app
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /telegram-bot
+  echo "Cloning custom repository from $UPSTREAM_REPO..."
+  git clone $UPSTREAM_REPO /app
 fi
-
-# Change directory to the bot folder
-cd /telegram-bot
-
-# Install dependencies from requirements.txt
-pip3 install -U -r requirements.txt
-
-# Start the bot
-echo "Starting Telegram Bot..."
+cd /app
+pip3 install -r requirements.txt
+echo "Starting bot..."
 python3 bot.py
