b91899ee02ebc76958e42245e88ac1f00906e9c3
shamilshaz03 <93109099+shamilshaz03@users.noreply.github.com>
2024-09-17 10:06:20 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Telegram Bot Token from @BotFather
+TOKEN = 'YOUR_BOT_API_TOKEN'
+
+# Admin configuration
+ADMIN_ID = 'YOUR_TELEGRAM_USER_ID'
+
+# Payment links
+GPAY_URL = 'https://gpay.payment.link'
+PAYTM_URL = 'https://paytm.payment.link'
+PHONEPE_URL = 'https://phonepe.payment.link'
